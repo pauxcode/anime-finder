6d9e8b6811bf605327017fef46730d51931ad2ba
DazaiLux <proyectpag@gmail.com>
2020-07-22 19:46:40 -0500

    Updates

---
+++ precache-manifest.c566e86bfed3c0b110a654fdc654f60e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ea79050645cf9504808d89a41d0a56",
+    "url": "/AinzOwn/anime-finder/index.html"
+  },
+  {
+    "revision": "f0aa66eb3ca0331d6487",
+    "url": "/AinzOwn/anime-finder/static/css/main.73a8b47e.chunk.css"
+  },
+  {
+    "revision": "70dd794a91bf774e9f64",
+    "url": "/AinzOwn/anime-finder/static/js/2.869cbaf8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AinzOwn/anime-finder/static/js/2.869cbaf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0aa66eb3ca0331d6487",
+    "url": "/AinzOwn/anime-finder/static/js/main.3fbbcb73.chunk.js"
+  },
+  {
+    "revision": "1bc652647844c359e439",
+    "url": "/AinzOwn/anime-finder/static/js/runtime-main.2d5b4de3.js"
+  }
+]);
